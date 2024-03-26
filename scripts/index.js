f89eec3e2b81e7946198954c036c776be6204651
Marina Balashova <deine.kleine.liebe@gmail.com>
2024-03-26 22:56:05 +0300

    minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,10 @@ const placesList = document.querySelector('.places__list');
 
 function addCard(cardData) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
     cardElement.querySelector('.card__image').src = cardData.link;
     cardElement.querySelector('.card__image').alt = cardData.name;
     cardElement.querySelector('.card__title').textContent = cardData.name;
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
     cardDeleteButton.addEventListener('click', function() {
         deleteCard(cardElement)
     }); 
