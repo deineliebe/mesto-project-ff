1b8aaaff268c4da355b6e2961362bd27e99039a6
Marina Balashova <deine.kleine.liebe@gmail.com>
2024-03-26 23:16:28 +0300

    Small fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
-const placesList = document.querySelector('.places__list');
+const places = document.querySelector('.places__list');
 
 function addCard(cardData, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
     cardElement.querySelector('.card__image').src = cardData.link;
     cardElement.querySelector('.card__image').alt = cardData.name;
     cardElement.querySelector('.card__title').textContent = cardData.name;
-    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
     cardDeleteButton.addEventListener('click', function() {
         deleteCard(cardElement)
     }); 
@@ -18,6 +18,6 @@ function deleteCard(card) {
     card.remove();
 };
 
-for (let i = 0; i < initialCards.length; i++) {
-    placesList.append(addCard(initialCards[i], deleteCard));
+for (initialCard of initialCards) {
+    places.append(addCard(initialCard, deleteCard));
 }
