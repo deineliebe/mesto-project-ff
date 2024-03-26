3c93fd0772ab703e7be5585e9c9a147caadfa8a4
Marina Balashova <deine.kleine.liebe@gmail.com>
2024-03-26 22:55:09 +0300

    initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function addCard(cardData) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardDeleteButton.addEventListener('click', function() {
+        deleteCard(cardElement)
+    }); 
+    return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+};
 
-// @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+    placesList.append(addCard(initialCards[i]));
+}
